7027a6f7713e2b52259239a3eced42ef544817d3
Benjamin Weigel <benjamin.weigel@europace.de>
2017-04-24 16:52:17 +0200

    [airflow] fixes permissions in docker container

--- airflow/mnt/entrypoint.sh
+++ airflow/mnt/entrypoint.sh
@@ -17,7 +17,7 @@ TRY_LOOP="20"
 
 # Load DAGs exemples (default: Yes)
 if [ "$LOAD_EX" = "n" ]; then
-    sed -i "s/load_examples = True/load_examples = False/" "$AIRFLOW_HOME"/airflow.cfg
+    sed -i "s/load_examples = True/load_examples = False/" ${AIRFLOW_HOME}/airflow.cfg
 fi
 
 # Install custome python package if requirements.txt is present
@@ -26,7 +26,7 @@ if [ -e "/requirements.txt" ]; then
 fi
 
 # Update airflow config - Fernet key
-sed -i "s|\$FERNET_KEY|$FERNET_KEY|" "$AIRFLOW_HOME"/airflow.cfg
+sed -i "s|\$FERNET_KEY|$FERNET_KEY|" ${AIRFLOW_HOME}/airflow.cfg
 
 # Wait for Postresql
 if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
@@ -60,9 +60,9 @@ then
       sleep 5
     done
   fi
-  sed -i "s#celery_result_backend = db+postgresql://airflow:airflow@postgres/airflow#celery_result_backend = db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#broker_url = redis://redis:6379/1#broker_url = redis://$REDIS_HOST:$REDIS_PORT/1#" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s#celery_result_backend = db+postgresql://airflow:airflow@postgres/airflow#celery_result_backend = db+postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" ${AIRFLOW_HOME}/airflow.cfg
+  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" ${AIRFLOW_HOME}/airflow.cfg
+  sed -i "s#broker_url = redis://redis:6379/1#broker_url = redis://$REDIS_HOST:$REDIS_PORT/1#" ${AIRFLOW_HOME}/airflow.cfg
   if [ "$1" = "webserver" ]; then
     echo "Initialize database..."
     $CMD initdb
@@ -73,9 +73,9 @@ then
   fi
 elif [ "$EXECUTOR" = "Local" ]
 then
-  sed -i "s/executor = CeleryExecutor/executor = LocalExecutor/" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#broker_url = redis://redis:6379/1#broker_url = redis://$REDIS_HOST:$REDIS_PORT/1#" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s/executor = CeleryExecutor/executor = LocalExecutor/" ${AIRFLOW_HOME}/airflow.cfg
+  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = postgresql+psycopg2://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB#" ${AIRFLOW_HOME}/airflow.cfg
+  sed -i "s#broker_url = redis://redis:6379/1#broker_url = redis://$REDIS_HOST:$REDIS_PORT/1#" ${AIRFLOW_HOME}/airflow.cfg
   echo "Initialize database..."
   $CMD initdb
   exec $CMD webserver &
@@ -86,8 +86,8 @@ else
     exec $CMD version
     exit
   fi
-  sed -i "s/executor = CeleryExecutor/executor = SequentialExecutor/" "$AIRFLOW_HOME"/airflow.cfg
-  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = sqlite:////usr/local/airflow/airflow.db#" "$AIRFLOW_HOME"/airflow.cfg
+  sed -i "s/executor = CeleryExecutor/executor = SequentialExecutor/" ${AIRFLOW_HOME}/airflow.cfg
+  sed -i "s#sql_alchemy_conn = postgresql+psycopg2://airflow:airflow@postgres/airflow#sql_alchemy_conn = sqlite:////usr/local/airflow/airflow.db#" ${AIRFLOW_HOME}/airflow.cfg
   echo "Initialize database..."
   $CMD initdb
   exec $CMD webserver
